0b7859dc54ee84ba60d2767d9d89e8ffa1d47854
ZackaryW <zackaryxcwang@gmail.com>
2022-07-20 12:36:21 -0700

    env: setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ast-plus',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     # pre-alpha
     classifiers=[
@@ -19,5 +19,6 @@ setup(
     author='Zackary W',
     description='ast-plus is an extension to ast that allows easier modification of ast nodes',
     url='https://github.com/ZackaryW/astplus',
-    
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
